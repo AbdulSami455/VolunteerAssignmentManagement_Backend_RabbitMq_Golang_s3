bb6755fe813615a85dc3bd02c112f4dac3f81af7
AbdulSami <msami.bese22seecs@student.nust.edu.pk>
2024-06-16 20:39:31 +0500

    COnnecting go app with rabbitmq server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AbdulSami455/volunteer-assignment-management
 
 go 1.22.4
+
+require github.com/streadway/amqp v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
 func main() {
 
-	println("Hello world")
+	fmt.Println("GO RabbitMq Tutorial")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Successfully Connected to our RabbitMQ Instance")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare("TestQueue", false, false, false, false, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println(q)
 
 }
