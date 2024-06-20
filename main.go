aeab55c353192755707da380fa179d901f528008
Abdul Sami <msami.bese22seecs@student.nust.edu.pk>
2024-06-20 14:52:34 +0500

    checking publisher

--- main.go
+++ main.go
@@ -10,9 +10,10 @@ func main() {
 	defer conn.Close()
 	defer ch.Close()
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 2; i++ {
 		publishMessage(ch, q, "Hello World!")
 	}
+
 	numConsumers := 3
 	for i := 1; i <= numConsumers; i++ {
 		go ConsumeMessages(ch, q, i)
