c384bff5f72d4eb033e9a2685033e754860c480a
Abdul Sami <msami.bese22seecs@student.nust.edu.pk>
2024-06-20 14:15:39 +0500

    working on consumer

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/streadway/amqp"
 )
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
 
 func main() {
 
 	fmt.Println("GO RabbitMq Tutorial")
 
+	// Establishing Connection
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
 		fmt.Println("Failed to connect to RabbitMQ:", err)
@@ -18,14 +26,14 @@ func main() {
 	defer conn.Close()
 
 	fmt.Println("Successfully connected to RabbitMQ")
-
+	// Establishing Channel
 	ch, err := conn.Channel()
 	if err != nil {
 		fmt.Println("Failed to open a channel:", err)
 		panic(err)
 	}
 	defer ch.Close()
-
+	// Declaring Queue
 	q, err := ch.QueueDeclare(
 		"TestQueue",
 		false,
@@ -41,6 +49,7 @@ func main() {
 
 	fmt.Printf("Queue declared: %v\n", q)
 
+	// Publishing message to the queue
 	body := "Hello World"
 	err = ch.Publish(
 		"",
@@ -57,4 +66,28 @@ func main() {
 	}
 
 	fmt.Printf("Successfully published message: %s\n", body)
+
+	// Register a consumer
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	// Create a channel to handle messages
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
