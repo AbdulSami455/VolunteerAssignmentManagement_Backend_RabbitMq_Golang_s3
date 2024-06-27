a4fe3c1d3314ba3f538068a2b68f36340381f4ac
Abdul Sami <msami.bese22seecs@student.nust.edu.pk>
2024-06-27 16:33:52 +0500

    web sockets handle correctly

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/AbdulSami455/volunteer-assignment-management
 
 go 1.22.4
 
-require github.com/streadway/amqp v1.1.0 // indirect
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/streadway/amqp v1.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,14 +3,41 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(p))
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Homepage")
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Websocket Endpoint")
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	reader(ws)
 }
 
 func setuproutes() {
@@ -49,6 +76,6 @@ func main() {
 
 	fmt.Println("Go Websockets")
 	setuproutes()
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8070", nil)
 
 }
